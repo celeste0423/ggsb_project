290654bbdcfe3df0bf96453d12f5cda8b5e9789c
celeste0423 <jyeob25@gmail.com>
2024-01-22 13:12:47 +0900

    0.0.1+1 new app feature

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:ggsb_project/root.dart';
-import 'package:ggsb_project/theme/base_theme.dart';
+import 'package:ggsb_project/src/root.dart';
+import 'package:ggsb_project/src/theme/base_theme.dart';
 
 import 'firebase_options.dart';
 
@@ -33,8 +33,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
