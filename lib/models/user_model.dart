ffce27a5ac684130ce96d7c2a9c7f3a6ff6c3163
celeste0423 <jyeob25@gmail.com>
2024-01-22 12:56:09 +0900

    0.0.1+1 base setting

---
+++ lib/models/user_model.dart
@@ -0,0 +1,8 @@
+class UserModel {
+  final String? uid;
+  //Todo: 여기에 이어서 내용들 어떤 거 들어가면 좋을 지 고민
+
+  UserModel({
+    this.uid,
+  });
+}
---
+++ lib/root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatelessWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
