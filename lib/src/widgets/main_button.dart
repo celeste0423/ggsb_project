02c7f21e2eace67a458ee00b27a94dd1b993a9dd
celeste0423 <jyeob25@gmail.com>
2024-01-23 14:52:38 +0900

    0.0.1+1 button widget

---
+++ lib/src/widgets/main_button.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class MainButton extends StatelessWidget {
+  String buttonText;
+  VoidCallback onTap;
+  Color? buttonColor;
+  Color? textColor;
+  double? width;
+  double? height;
+
+  MainButton({
+    Key? key,
+    required this.buttonText,
+    required this.onTap,
+    this.buttonColor,
+    this.textColor,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height ?? 45,
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: buttonColor ?? CustomColors.mainPink,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(0, 0),
+                blurRadius: 5,
+              ),
+            ]),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              color: textColor ?? Colors.white,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Widget mainButton(
+//   String text,
+//   Function()? onPressed,
+//     [double? x, Color? color]
+// ) {
+//   return SizedBox(
+//     height: 50,
+//     width: x ?? double.infinity,
+//     child: ElevatedButton(
+//       style: ElevatedButton.styleFrom(
+//         backgroundColor: color, // Background color
+//       ),
+//       onPressed: onPressed,
+//       child: Text(
+//         text,
+//         style: TextStyle(fontSize: 35, color: Colors.white),
+//       ),
+//     ),
+//   );
+// }
