7ca051c33d737d7b14a294991a6fffdf0e5d6f2f
Minjae079 <k0709075@gmail.com>
2024-02-14 17:57:44 +0900

    캘린더 ondaySelected수정

--- lib/src/features/my/data_analyze/controller/data_analyze_controller.dart
+++ lib/src/features/my/data_analyze/controller/data_analyze_controller.dart
@@ -2,4 +2,11 @@ import 'package:get/get.dart';
 
 class DataAnalyzePageController extends GetxController {
   static DataAnalyzePageController get to => Get.find();
+
+  Rx<DateTime> selectedDate = DateTime.now().obs;
+
+  void onDaySelected(DateTime selectedDateTime){
+    selectedDate(selectedDateTime);
+  }
+
 }
--- lib/src/features/my/data_analyze/page/data_analyze_page.dart
+++ lib/src/features/my/data_analyze/page/data_analyze_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ggsb_project/src/features/my/data_analyze/controller/data_analyze_controller.dart';
+import 'package:ggsb_project/src/utils/custom_color.dart';
+import 'package:ggsb_project/src/widgets/svg_icon_button.dart';
 import 'package:ggsb_project/src/widgets/title_text.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class DataAnalyzePage extends GetView<DataAnalyzePageController> {
   const DataAnalyzePage({super.key});
@@ -9,8 +12,32 @@ class DataAnalyzePage extends GetView<DataAnalyzePageController> {
   PreferredSizeWidget _appBar() {
     return AppBar(
       centerTitle: true,
+      leading: SvgIconButton(
+        assetPath: 'assets/icons/back.svg',
+        onTap: Get.back,
+      ),
       title: TitleText(
         text: '기록 분석',
+      ),
+    );
+  }
+
+  Widget _calendar() {
+    return Obx(
+      () => TableCalendar(
+        focusedDay: DateTime.now(),
+        currentDay: controller.selectedDate.value,
+        firstDay: DateTime(2024, 1, 1),
+        lastDay: DateTime(2030, 12, 31),
+        // locale: 'ko-KR',
+        daysOfWeekHeight: 30,
+        onDaySelected: (selectedDateTime,focusedDay){
+          controller.onDaySelected(selectedDateTime);
+        },
+        calendarStyle: CalendarStyle(
+          todayDecoration: BoxDecoration(color: CustomColors.mainBlue, shape: BoxShape.circle),
+          selectedDecoration: BoxDecoration(color: const Color(0xFF00FF00), shape: BoxShape.circle),
+        ) ,
       ),
     );
   }
@@ -21,13 +48,8 @@ class DataAnalyzePage extends GetView<DataAnalyzePageController> {
     return Scaffold(
       appBar: _appBar(),
       body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Text('추후 구현 예정입니다'),
-        ),
+        child: _calendar(), // Calendar 위젯을 출력합니다.
       ),
     );
   }
-
-
 }
