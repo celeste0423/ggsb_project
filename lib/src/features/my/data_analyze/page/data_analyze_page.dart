b24ad21a6a3c3501ab965bd4df7e4ad071057d5f
Minjae079 <k0709075@gmail.com>
2024-02-15 10:56:57 +0900

    캘린더 ondaySelected수정중

--- lib/src/features/my/data_analyze/page/data_analyze_page.dart
+++ lib/src/features/my/data_analyze/page/data_analyze_page.dart
@@ -29,15 +29,54 @@ class DataAnalyzePage extends GetView<DataAnalyzePageController> {
         currentDay: controller.selectedDate.value,
         firstDay: DateTime(2024, 1, 1),
         lastDay: DateTime(2030, 12, 31),
-        // locale: 'ko-KR',
+        locale: 'ko-KR',
         daysOfWeekHeight: 30,
+
+
+        selectedDayPredicate: (day) {
+          return isSameDay(controller.selectedDate.value, day);
+        },
         onDaySelected: (selectedDateTime,focusedDay){
           controller.onDaySelected(selectedDateTime);
         },
+
+
+        // onDaySelected: (selectedDay, focusedDay) {
+        //     selectedDay = selectedDay;
+        //     focusedDay = focusedDay; // update `_focusedDay` here as well
+        // },
+
         calendarStyle: CalendarStyle(
-          todayDecoration: BoxDecoration(color: CustomColors.mainBlue, shape: BoxShape.circle),
-          selectedDecoration: BoxDecoration(color: const Color(0xFF00FF00), shape: BoxShape.circle),
-        ) ,
+            defaultTextStyle: TextStyle(color: Colors.grey,),
+            weekendTextStyle: TextStyle(color: Colors.redAccent),
+            outsideDaysVisible: false,
+            todayDecoration: BoxDecoration(
+                color: Colors.transparent,
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.green,width: 1.5)
+            ),
+            todayTextStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+            )
+        ),
+
+        eventLoader: (day){
+          if (day.day%2==0){
+            return ['hi'];
+          }
+          return [];
+        },
+
+        // calendarStyle: CalendarStyle(
+        //   todayDecoration: BoxDecoration(color: CustomColors.mainBlue, shape: BoxShape.circle),
+        //   selectedDecoration: BoxDecoration(color: const Color(0xFF00FF00), shape: BoxShape.circle),
+        // ) ,
+
+        headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+        ),
       ),
     );
   }
